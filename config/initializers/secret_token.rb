a5fa4a80de40681bbfb490d421137573dc3f77a2
Lydia Kwag <wag0325@gmail.com>
2014-01-21 14:03:14 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KoreaApp::Application.config.secret_key_base = '2a622ad695e704fc25f2577e74f3c05c36640bd47fc22a7877038dbfd763f10881704459b174f0cc98a22a61952f4d7768a8da093b055d37a62b49e1d0dd6296'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KoreaApp::Application.config.session_store :cookie_store, key: '_korea_app_session'
